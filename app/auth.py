67e7d6b3303bfb9197dc4f2b7486f2f934a07d9b
Dev <devsapariya94@gmail.com>
2024-03-26 15:53:51 +0530

    added authentication part including login register and logout

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,10 @@ from flask import Blueprint, request, jsonify
 from . import models
 from . import views
 import jwt
-
+from functools import wraps
+import datetime
+from datetime import timedelta
+import hashlib
 
 auth = Blueprint('auth', __name__)
 
@@ -10,41 +13,111 @@ from . import views
 from . import models    
 from . import config
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return jsonify({'error': 'Token is invalid'}), 401
+      
+        if models.BlacklistToken.find_one(token):
+            return jsonify({'error': 'Token is invalid'}), 401
+
+        try:
+            data = jwt.decode(token, config.Config.SECRET_KEY, algorithms=['HS256'])
+            current_user = data['email']
+        except jwt.ExpiredSignatureError:
+            return jsonify({'error': 'Token is invalid'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'error': 'Token is invalid'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+
+def generate_token(user):
+    payload = {
+        'email': user['email'],  
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    }
+    token = jwt.encode(payload, config.Config.SECRET_KEY, algorithm='HS256')
+    return token
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+    
 
 @auth.route('/login')
 def login():
-        return 'Login'
+        request_data = request.get_json()   
+        email = request_data['email']
+        password = request_data['password']
+        password = hash_password(password)
+        user = models.User.get_user(email)
+        if user:
+            if user['password'] == password:
+                token = generate_token(user)
+                return jsonify({'token': token, 
+                                "status" : "success"}), 200
+            else:
+                return {
+                    'message': 'Invalid email or password',
+                    'status': 'error'
+                }, 406
+        else:
+            return {
+                'message': 'Invalid email or password',
+                'status': 'error'
+            }, 406
+
+
 
 @auth.route('/logout')
-def logout():
-    return 'Logout'
+@token_required
+def logout(current_user):
+    token = request.headers.get('Authorization')
+    models.BlacklistToken.save(token)
+    return jsonify({'message': 'Successfully logged out'}), 200
+
 
 @auth.route('/register', methods=['POST'])
 def register():
     request_data = request.get_json()
-    username = request_data['username']
+    name = request_data['name']
     password = request_data['password']
+    password = hash_password(password)
     email = request_data['email']
     try: 
-        if models.User.get_user(username):
+        if models.User.get_user(email):
             return_data = {
-                'message': 'User already exists',
+                'message': 'Email already exists',
                 "status": "error"
-            }
+            }, 406
             return return_data
         else:
-            user = models.User(username, password, email)
+            user = models.User(name, password, email)
+
             user.save()
+            temp_json = {
+                'name': name,
+                'email': email
+            }
+            token= generate_token(temp_json)
             return_data = {
+                "token": token,
                 'message': 'User created',
                 "status": "success"
-            }
+            }, 200
             return return_data 
 
-    except:
+    except Exception as e:
         return_data = {
             'message': 'An error occurred',
             "status": "error"
-        }
+        }, 406
         return return_data
     
--- app/models.py
+++ app/models.py
@@ -5,21 +5,32 @@ from . import config
 client = MongoClient(config.MONGO_URI)
 db = client[config.DB_NAME]
 users_collection = db[config.USER_COLLECTION]
-
+blacklistToken_collection = db[config.BLACKLIST_TOKEN_COLLECTION]
 
 class User:
-    def __init__(self, username, password, email):
-        self.username = username
+    def __init__(self, name, password, email):
+        self.name = name
         self.password = password
         self.email = email
 
     def save(self):
         users_collection.insert_one({
-            'username': self.username,
+            'name': self.name,
             'password': self.password,
             'email': self.email
         })  
       
-    def get_user(username):
-        user = users_collection.find_one({'username': username})
+    def get_user(email):
+        user = users_collection.find_one({'email': email})
         return user
+    
+class BlacklistToken:
+   
+    def save(token):
+        blacklistToken_collection.insert_one({
+            'token': token
+        })
+
+    def find_one(token):
+        return blacklistToken_collection.find_one({'token': token})
+    
