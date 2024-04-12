78ccbd0ea44a0269dd9365b1aa0cda92317078c6
Dev <devsapariya94@gmail.com>
2024-04-12 15:53:40 +0530

    Added the routes for the project creation and updation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import math
 from . import config    
+import random
 
 client = MongoClient(config.MONGO_URI)
 db = client[config.DB_NAME]
@@ -59,3 +60,155 @@ class BlacklistToken:
 
     def find_one(token):
         return blacklistToken_collection.find_one({'token': token})
+    
+
+def get_unique_project_id():
+        
+        project_id = random.randint(10000, 9999999 )
+        while project_collection.find_one({'project_id': project_id}):
+            project_id = random.randint(100000, 999999)
+        return project_id
+
+
+class Project:
+    def __init__(self, title, description, purpose, owner, mentor, duration, team = [], objectives = None, documents = None, requirements= None, tasks = None):
+        self.project_id = get_unique_project_id()
+        self.title = title
+        self.description = description
+        self.purpose = purpose
+        self.owner = owner
+        self.mentor = mentor
+        self.duration = duration
+        self.team = team
+        self.objectives = objectives
+        self.documents = documents
+        self.requirements = requirements
+        self.tasks = tasks
+
+    def save(self):
+        project_collection.insert_one({
+            'project_id': self.project_id,
+            'title': self.title,
+            'description': self.description,
+            'purpose': self.purpose,
+            'owner': self.owner,
+            'mentor': self.mentor,
+            'duration': self.duration,
+            'team': self.team,
+            'objectives': self.objectives,
+            'documents': self.documents,
+            'requirements': self.requirements,
+            'tasks': self.tasks
+        })
+    
+        return self.project_id
+    
+    
+    def get_project(project_id):
+        project = project_collection.find_one({'project_id': project_id})
+        return project
+    
+    def get_project_documents(project_id):
+        project = project_collection.find_one({'project_id': project_id})
+        
+        if not project:
+            return {'message': 'Project not found', 'status': 'failed'}
+        
+        return project['documents']
+        
+    def get_project_by_owner(owner):
+        # find all projects owned by the user
+        project = project_collection.find({'owner': owner})
+        return project
+    
+    def get_project_by_mentor(mentor):
+        project = project_collection.find({'mentor': mentor})
+        return project
+    
+    def get_project_by_team_member(team_member):
+        project = project_collection.find_one({'team': team_member})
+        return project
+    
+    def add_task(project_id, task):
+        a = {'project_id': project_id}
+        b = {'$push': {'tasks': task}}
+        project_collection.update_one(a,b)
+
+    def get_tasks(project_id):
+        project = project_collection.find_one({'project_id': project_id})
+        return project['tasks']
+
+    def update_project_documents(project_id, documents):
+        a = {'project_id': project_id}
+        b = {'$set': {'documents': documents}}
+        project_collection.update_one(a,b)
+        
+
+def get_unique_task_id(): 
+        task_id = random.randint(10000, 9999999 )
+        while task_collection.find_one({'task_id': task_id}):
+            task_id = random.randint(100000, 999999)
+        return task_id
+    
+    
+class Task:
+    def __init__ (self, title, duration,assignee, project_id,  status = None, priority = None, subtask = None, tags = None, description = None):
+        self.task_id = get_unique_task_id()
+        self.title = title
+        self.duration = duration
+        self.assignee = assignee
+        self.status = status
+        self.priority = priority
+        self.subtask = subtask
+        self.tags = tags
+        self.description = description
+        self.project_id = project_id
+
+    def save(self):
+        task_collection.insert_one({
+            'task_id': self.task_id,
+            'title': self.title,
+            'duration': self.duration,
+            'assignee': self.assignee,
+            'status': self.status,
+            'priority': self.priority,
+            'subtask': self.subtask,
+            'tags': self.tags,
+            'description': self.description,
+            'project_id': self.project_id
+        })
+
+        Project.add_task(self.project_id, self.task_id)
+
+    def get_tasks_by_project(project_id):
+        tasks = task_collection.find({'project_id': project_id})
+        return tasks
+    
+    def get_task(task_id):
+        task = task_collection.find_one({'task_id': task_id})
+        return task
+
+    def update_task_status(task_id, status):
+        a = {'task_id': task_id}
+        b = {'$set': {'status': status}}
+        task_collection.update_one(a,b)
+
+    def update_task_priority(task_id, priority):
+        a = {'task_id': task_id}
+        b = {'$set': {'priority': priority}}
+        task_collection.update_one(a,b)
+
+    def update_task_subtask(task_id, subtask):
+        a = {'task_id': task_id}
+        b = {'$set': {'subtask': subtask}}
+        task_collection.update_one(a,b)
+
+    def update_task_tags(task_id, tags):
+        a = {'task_id': task_id}
+        b = {'$set': {'tags': tags}}
+        task_collection.update_one(a,b)
+
+    def get_task_by_assignee(assignee):
+        task = task_collection.find_one({'assignee': assignee})
+        return task
+    
--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,12 @@ def uploadToFirebase(data, use, filename):
                 
         bucket = storage.bucket(app=firebase)
         blob = bucket.blob(f'{use}/{filename}')
-        blob.upload_from_string(data, content_type='image/png')
+        if use == 'project-documents':
+            blob.upload_from_string(data, content_type='application/pdf')
+        
+        elif use == 'profile-pictures':
+            blob.upload_from_string(data, content_type='image/png')
+
         blob.make_public()
         return blob.public_url
     
@@ -91,3 +96,118 @@ def all_user_data(current_user):
         'skills': user['skills']
     }
     return jsonify(data), 200
+
+@routes.route('/create-project', methods=['POST'])
+@auth.token_required
+def create_project(current_user):
+    team = []
+    objectives = None
+    documents = None
+    requirements = None
+    tasks = None
+    
+    data = request.get_json()
+    title = data['title']
+    description = data['description']
+    purpose = data['purpose']
+    owner = current_user
+    mentor = data['mentor']
+    duration = data['duration']
+    if 'team' in data:
+        team = data['team']
+    elif 'objectives' in data:
+        objectives = data['objectives']
+    elif 'documents' in data:
+        documents = data['documents']
+    elif 'requirements' in data:
+        requirements = data['requirements']
+    elif 'tasks' in data:
+        tasks = data['tasks']
+    
+    project_id = models.Project(title, description, purpose, owner, mentor, duration, team, objectives, documents, requirements, tasks).save()
+
+    return jsonify({'message': 'Project created', 
+                    'project_id': project_id, 'status': 'success'}), 200
+
+@routes.route('/get-project', methods=['GET'])
+@auth.token_required
+def get_project(current_user):
+    project_id = request.get_json()['project_id']
+    project = models.Project.get_project(int(project_id))
+
+    # check if user is the owner or in the team
+    if project['owner'] != current_user and current_user not in project['team']:
+        return jsonify({'message': 'You are not allowed to view this project', 'status': 'failed'}), 403
+
+    # convert the project id to string
+    project["_id"] = str(project["_id"])
+    return jsonify(project), 200
+
+@routes.route('/get-project-documents', methods=['GET'])
+def get_project_documents():
+    project_id = request.get_json()['project_id']
+    documents = models.Project.get_project_documents(int(project_id))
+    
+    try: 
+        if documents['status'] == 'failed':
+            return jsonify(documents), 404
+    except:
+        pass
+    if not documents:
+        return jsonify({'message': 'No document found', 'status': 'success'}), 200
+    print(documents)
+    return jsonify(documents), 200
+
+
+@routes.route('/get-project-by-owner', methods=['GET'])
+@auth.token_required
+def get_project_by_owner(current_user):
+    owner = current_user
+    print(owner)
+    projects = models.Project.get_project_by_owner(owner)
+    
+    # projects is in mongoDB cursor object so we need to convert it to list
+    projects = list(projects)
+
+    # modify the project _id to string
+    for project in projects:
+        project["_id"] = str(project["_id"])
+
+
+    return jsonify(projects), 200
+
+
+
+@routes.route('/get-project-by-mentor', methods=['GET'])
+def get_project_by_mentor():
+    mentor = request.get_json()['mentor']
+    projects = models.Project.get_project_by_mentor(mentor)
+
+    projects = list(projects)
+
+    for project in projects:
+        project["_id"] = str(project["_id"])
+
+    return jsonify(projects), 200
+
+
+@routes.route('/add-project-document', methods=['POST'])
+@auth.token_required
+def add_project_document(current_user):
+    project_id = request.form['project_id']
+    document = request.files['document']
+    filename = document.filename
+
+    project = models.Project.get_project(int(project_id))
+    if not project:
+        return jsonify({'message': 'Project not found', 'status': 'failed'}), 404
+    
+    if project['owner'] != current_user and current_user not in project['team']:
+        return jsonify({'message': 'You are not allowed to add document to this project', 'status': 'failed'}), 403
+    
+    filename = f"{project_id}.pdf"
+
+    public_url = uploadToFirebase(document, 'project-documents', filename)
+    
+    models.Project.update_project_documents(int(project_id), public_url)
+    return jsonify({'message': 'Document added','url':public_url, 'status': 'success'}), 200
